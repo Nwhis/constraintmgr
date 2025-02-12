c4a056c7b6c1fb19323d74cb5faf1f51f6c00243
Nwhis <17502190+Nwhis@users.noreply.github.com>
2025-02-11 23:32:54 -0600

    test potential fix for render

--- lua/constraintmgr/cl_render.lua
+++ lua/constraintmgr/cl_render.lua
@@ -84,6 +84,8 @@ local Think = function() -- Calculating constraint worldpos and relative tooltip
                 w.WPos1 = (w.Ent1:IsWorld() and w.LPos1 == vector_origin) and (w.Ent2:GetPos() + Vector(0,0,-32)) or w.Ent1:LocalToWorld(w.LPos1)
                 w.WPos2 = (w.Ent2:IsWorld() and w.LPos2 == vector_origin) and (w.Ent1:GetPos() + Vector(0,0,-32)) or w.Ent2:LocalToWorld(w.LPos2)
             end
+            v.WPos1 = v.WPos1 and v.WPos1 or vector_origin
+            v.WPos2 = v.WPos2 and v.WPos2 or vector_origin
         end
         if (not CMgr_FreezeRender or not v.WPos) and v.WPos1 then
             v.WPos = ((v.WPos1 + v.WPos2) * 0.5)
